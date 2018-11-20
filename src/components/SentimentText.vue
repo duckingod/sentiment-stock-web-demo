ba92c621013986ea39a6415af70bafa3ea2eee06
duck <kingoduck@gmail.com>
2018-11-21 01:05:10 +0800

    Fix sentiment color

--- src/components/SentimentText.vue
+++ src/components/SentimentText.vue
@@ -1,11 +1,10 @@
 <template>
   <span>
     <template v-for="(t, i) in texts">
-      aa
       <span
         :key="i"
         :style="{color: t.color}"
-      >{{ t.text }} {{ t.color }}</span>
+      >{{ t.text + ' ' }} </span>
     </template>
   </span>
 </template>
@@ -22,14 +21,14 @@ export default {
   computed: {
     texts() {
       return this.text.split(' ').map(t => {
-        let color = "000"
+        let color = "#000"
         t = t.toLowerCase()
         if (sentiment[t]) {
           // rgb - 000 - FF0 (positive) - 00F (negetive)
           let s = Math.round(sentiment[t] / 8) + 2
           let c1 = Math.min(Math.abs(s), 15).toString(16)
-          let c2 = Math.round(Math.min(Math.abs(s), 15) / 2).toString(16)
-          color = "#" + (s > 0 ? c1 + "00" : "00" + c1)
+          let c2 = Math.round(Math.min(Math.abs(s), 12) / 2).toString(16)
+          color = "#" + (s > 0 ? `0${c2}0` : `${c1}00`)
         }
         return {
           text: t,
