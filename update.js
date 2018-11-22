c76acb45030728939f2074890dc399556d26fc1d
duckingod <kingoduck@gmail.com>
2018-11-22 16:00:45 +0800

    Periodic update json XDD

---
+++ update.js
@@ -0,0 +1,12 @@
+const fs = require('fs')
+const copy = (src, tar) => fs.createReadStream(src).pipe(fs.createWriteStream(tar));
+path = process.env.DATA_PATH
+if (path) {
+  setInterval(() => {
+    console.log(new Date())
+      console.log('Copied!')
+    copy(path, './src/latest_demo_info.json')
+  }, 300 * 1e3)
+} else {
+  console.log('No env var: DATA_PATH QQ')
+}
