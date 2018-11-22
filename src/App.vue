90991cf00afd1c9c16adf39f13b3a2458c8bc308
duckingod <kingoduck@gmail.com>
2018-11-22 10:44:41 +0800

    Title: CrowdPT

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,8 @@
 <template>
   <v-app>
     <v-toolbar app>
-      <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
+      <v-toolbar-title class="headline">
+        <span>CrowdPT</span>
       </v-toolbar-title>
       <v-spacer />
       <v-btn
